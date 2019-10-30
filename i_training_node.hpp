4b369f613d0e57912f4ccbb701d2ea843d70ed17
Oleksandr Bieliakov <oleksandr.bieliakov@volvocars.com>
2019-10-30 11:45:03 +0200

    Changed interface for tree node accoring to new architecture
    Added functions to check dependecies and to add dependecies

--- i_training_node.hpp
+++ i_training_node.hpp
@@ -1,13 +1,17 @@
 #ifndef TRAINING_TREE_I_TRAINING_NODE_HPP_
 #define TRAINING_TREE_I_TRAINING_NODE_HPP_
 
-#include <memory>
-#include <vector>
+namespace training_tree {
 
 class ITrainingNode {
 public:
-  std::shared_ptr<std::vector<ITrainingNode>> Ancestors();
-  std::shared_ptr<std::vector<ITrainingNode>> Descendants();
+  virtual bool HasDependencies() const = 0;
+  virtual void AddDependency(const ITrainingNode& node) = 0;
+
+  //TODO: find a way to pass iterators to this function
+  virtual bool HasOtherDependencies() const = 0;
 };
+
+} // namespace training_tree
 
 #endif // TRAINING_TREE_I_TRAINING_NODE_HPP_
--- training_node.hpp
+++ training_node.hpp
@@ -1,19 +1,17 @@
 #ifndef TRAINING_TREE_TRAINING_NODE_HPP_
 #define TRAINING_TREE_TRAINING_NODE_HPP_
 
-#include <memory>
-#include <vector>
-
 #include "i_training_node.hpp"
+
+namespace training_tree {
 
 class TrainingNode : public ITrainingNode {
 public:
-  std::shared_ptr<std::vector<ITrainingNode>> Ancestors();
-  std::shared_ptr<std::vector<ITrainingNode>> Descendants();
-
-private:
-  std::shared_ptr<std::vector<ITrainingNode>> ancestors_;
-  std::shared_ptr<std::vector<ITrainingNode>> descendants_;
+  bool HasDependencies() const override;
+  void AddDependency(const ITrainingNode &node) override;
+  bool HasOtherDependencies() const override;
 };
+
+} // namespace training_tree
 
 #endif // TRAINING_TREE_TRAINING_NODE_HPP_
